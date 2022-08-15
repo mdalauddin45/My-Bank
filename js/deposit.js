c9ae10e9f46bc8e7fb6850eb15088005d5cec192
AlauddinTheSufiBhai <mdalauddin8948@gmail.com>
2022-08-16 01:34:05 +0600

    next

--- js/deposit.js
+++ js/deposit.js
@@ -3,6 +3,11 @@ document.getElementById('deposit-btn').addEventListener('click', function () {
     const depositField = document.getElementById('user-deposit');
     const newDepositAmountString = depositField.value;
     const newDepositAmount = parseFloat(newDepositAmountString);
+    if (isNaN(newDepositAmount)) {
+        alert('please provide a valid number')
+        return;
+    }
+
 
     const depositTotalElement = document.getElementById('total-deposit');
     const previousDepositTotalString = depositTotalElement.innerText;
@@ -37,13 +42,17 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
     const newWithdrewAmountString = withdrawField.value;
     const newWithdrewAmount = parseFloat(newWithdrewAmountString);
 
+    if (isNaN(newWithdrewAmount)) {
+        alert('please provide a valid number')
+        return;
+    }
+
+
     const previousWithdrew = document.getElementById('total-withdraw');
     const previousWithdrewString = previousWithdrew.innerText;
     const previousWithdrewTotal = parseFloat(previousWithdrewString);
 
-    const currentWithdrewTotal = newWithdrewAmount + previousWithdrewTotal;
 
-    previousWithdrew.innerText = currentWithdrewTotal;
 
     const balanceTotalElement = document.getElementById('total-balance');
     const previousBalanceStirng = balanceTotalElement.innerText;
@@ -51,11 +60,17 @@ document.getElementById('withdraw-btn').addEventListener('click', function () {
 
     if (newWithdrewAmount > previousBalanceTotal) {
         alert('Ami Fokir Ami Miskin');
+        return;
     }
 
-    const currentBalance = previousBalanceTotal - newWithdrewAmount;
+    const currentWithdrewTotal = newWithdrewAmount + previousWithdrewTotal;
+    previousWithdrew.innerText = currentWithdrewTotal;
 
+
+    const currentBalance = previousBalanceTotal - newWithdrewAmount;
     balanceTotalElement.innerText = currentBalance;
+
+
 
     // clear the withdrew field
     withdrawField.value = ``;
